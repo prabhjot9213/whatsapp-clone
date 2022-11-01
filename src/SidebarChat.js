87d16d1452515b4d910e0ea89fa7a9aecfd2c995
prabhjot <prabhjot@mapmyindia.com>
2022-11-01 15:30:56 +0530

    added avatars and logic for random avatars

--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import { Avatar } from '@mui/material';
+//import React, { useEffect } from 'react';
 import  './SidebarChat.css';
+import { useEffect, useState } from "react";
+//import { useEffect } from 'react';
+//import { useState } from 'react';
+//import { ReactDOM } from 'react';
 
 function SidebarChat() {
-  return (
+    const [seed,setSeed] = useState("");
+
+    useEffect(()=>{
+        setSeed(Math.floor(Math.random()*5000));
+    }, []);
+  
+    return (
     <div className='sidebarChat'>
-        <h1>Hi</h1>
-        
-    </div>
+        <Avatar src={`https://avatars.dicebear.com/api/open-peeps/${seed}.svg`}/>
+        <div className='sidebarChat__info'>
+            <h3>Room name</h3>
+            <p>Last message</p>
+        </div>
+
+
+
+                </div>
   )
 }
 
