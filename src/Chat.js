5174dad9a52222b410cf11fb20fbf51712e3074c
prabhjot <prabhjot@mapmyindia.com>
2022-11-09 12:45:22 +0530

    chat.js added hooks and buttons

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import './Chat.css';
+import { useEffect,useState } from "react";
+import { AttachFile, MoreVert, SearchOutlined } from '@mui/icons-material';
+import { Avatar, IconButton } from '@mui/material';
 
 function Chat() {
+    const [seed,setSeed] = useState("");
+    useEffect(()=>{
+        setSeed(Math.floor(Math.random()*5000));
+    }, []);
+
+
+
   return (
     <div className='chat'>
-        <div className='chat__header'></div>
+        <div className='chat__header'>
+        <Avatar src={`https://avatars.dicebear.com/api/open-peeps/${seed}.svg`}/>
+        <div className='chat__headerInfo'><h3>Room name</h3><p>last seen ...</p></div>
+        <div className='chat__headerRight'>
+            <IconButton><SearchOutlined /></IconButton>
+            <IconButton><AttachFile /></IconButton>
+            <IconButton><MoreVert /></IconButton>
+        </div>
+        </div>
         <div className='chat__body'></div>
+        <div className='chat__footer'></div>
     </div>
   )
 }
